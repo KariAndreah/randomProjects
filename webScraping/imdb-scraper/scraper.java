a945b9ffbbefa6b88e2378f1f60d07577a58041a
KariAndreah <kandreahburt@gmail.com>
2018-03-27 22:17:52 -0500

    Create scraper.java

---
+++ webScraping/imdb-scraper/scraper.java
@@ -0,0 +1,20 @@
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+
+public class Scraper {
+    public static void main(String [] args) throws Exception{
+
+        final Document document = Jsoup.connect("http://www.imdb.com/chart/top").get();
+
+        System.out.println(document.outerHtml());
+
+        for (Element row: document.select("table.chart.full-width tr")){
+
+            final String title = row.select(".titleColumn a").text();
+            final String rating = row.select(".imdbRating").text();
+
+            System.out.println(title + " -> Rating: " + rating);
+        }
+    }
+}
