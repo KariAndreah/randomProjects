f3b3aa8d29e6614a28c3c326ce8b6b2d7b22d756
KariAndreah <kandreahburt@gmail.com>
2018-03-31 00:36:53 -0500

    Update BattingStats.py
    Basic Web Scrapping program that uses BeautifulSoup and Requests to grab batting statistics from Houston Astros.

--- WebScraping/MLBBattingStats/BattingStats.py
+++ WebScraping/MLBBattingStats/BattingStats.py
@@ -3,6 +3,7 @@ Created on Fri Mar 30 13:28:52 2018
 
 @author: Kari
 """
+#Basic Web Scrapping program that uses BeautifulSoup and Requests to grab batting statistics from Houston Astros. 
 
 import requests
 from bs4 import BeautifulSoup 
